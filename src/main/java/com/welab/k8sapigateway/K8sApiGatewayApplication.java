0a5f5e7536a8ab66b41c63a3a47dfcd6e42c5277
Sunhokim2 <mcwright@naver.com>
2025-06-09 17:25:37 +0900

    w

---
+++ src/main/java/com/welab/k8sapigateway/K8sApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.welab.k8sapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K8sApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(K8sApiGatewayApplication.class, args);
+    }
+
+}
